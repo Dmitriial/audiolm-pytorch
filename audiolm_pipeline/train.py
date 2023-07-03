955563b824c54dc200b18cf04ecdffcbb3cac458
Dmitrii Kulik <d_kulik@wargaming.net>
2023-07-03 10:21:49 +0300

    Add possibility to laod a previous results.

--- audiolm_pipeline/train.py
+++ audiolm_pipeline/train.py
@@ -4,9 +4,10 @@ import torch
 import argparse
 from audiolm_pytorch import EncodecWrapper
 from audiolm_pytorch import SoundStream, SoundStreamTrainer
+from typing import Optional
 
 
-def train(wav_folder: Path):
+def train(wav_folder: Path, model_pt: Optional[Path] = None) -> None:
     encodec = EncodecWrapper()
     # Now you can use the encodec variable in the same way you'd use the soundstream variables below.
 
@@ -29,6 +30,11 @@ def train(wav_folder: Path):
         num_train_steps=1_000_000
     ).cuda()
 
+    if model_pt is not None:
+        print(f"Load model: {model_pt}")
+        trainer.load(str(model_pt))
+        print(f"Load model: {model_pt} OK")
+
     trainer.train()
 
     # after a lot of training, you can test the autoencoding as so
@@ -41,12 +47,18 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("--wav_folder", dest="wav_folder", type=Path, required=True,
                         help="Path to the folder with wav files (!)")
+    parser.add_argument("--pt", dest="model_pt", type=Path, required=False, default=None,
+                        help="Path to the model pt (!)")
 
     opt = parser.parse_args()
     wav_folder: Path = opt.wav_folder
     assert wav_folder.exists() and wav_folder.is_dir(), f"Must be exists: {wav_folder}!"
 
-    train(wav_folder=wav_folder)
+    model_pt: Path = opt.model_pt
+    if model_pt is not None:
+        assert model_pt.exists(), f"Model pt path must be exists: {model_pt} (!)"
+
+    train(wav_folder=wav_folder, model_pt=model_pt)
 
 
 if __name__ == '__main__':
