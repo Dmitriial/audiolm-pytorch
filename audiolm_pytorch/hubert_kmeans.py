4037b0e3d707e3ef13d42e3b02319fd7059f7d2c
Dmitrii Kulik <d_kulik@wargaming.net>
2023-09-03 16:38:50 +0300

    Revert "fix hubert wav2vec"
    This reverts commit cb9ed8520d51e6b59f9e2345bc228ef46a6e64fc.

--- audiolm_pytorch/hubert_kmeans.py
+++ audiolm_pytorch/hubert_kmeans.py
@@ -97,9 +97,8 @@ class HubertWithKmeans(nn.Module):
 
         codebook_indices = torch.from_numpy(codebook_indices).to(device).long()
 
-        codebook_indices, = unpack(codebook_indices, packed_shape, '*')
-
         if flatten:
             return codebook_indices
 
-        return rearrange(codebook_indices, 'b ... -> b (...)')
+        codebook_indices, = unpack(codebook_indices, packed_shape, '*')
+        return codebook_indices
